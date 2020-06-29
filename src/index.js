4feaf31a80c4d8b628d8edd46fa59075f35193e9
Matheus <theus-7@hotmail.com>
2020-06-29 16:28:21 -0300

    studying base ui drawer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {BaseProvider, DarkTheme} from 'baseui';
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+import Example from "./example";
+
+const engine = new Styletron();
+
+function App() {
+  return <Example />;
+}
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={DarkTheme}>
+      <App />
+    </BaseProvider>
+  </StyletronProvider>,
+  rootElement
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
